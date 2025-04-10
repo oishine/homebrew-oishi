7486115c30665b7f9c79df9748485c69f2fae0db
neroices <git@slce.moe>
2025-04-10 19:31:31 +0700

    Initial

---
+++ Casks/pcsx2.rb
@@ -0,0 +1,15 @@
+cask "pcsx2" do
+  version "2.3.260"
+  sha256 "9ffd0f03899ecc3746edb2078d38b53f16eaca831fc0dc78c8988f5d9991f77d"
+  url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macos-Qt.tar.xz"
+  name "PCSX2"
+  desc "Open Source PS2 Emulator"
+  homepage "https://pcsx2.net"
+
+  app "PCSX2-v#{version}.app", target: "PCSX2.app"
+
+  zap trash: [
+    "~/Library/Preferences/net.pcsx2.pcsx2.plist",
+    "~/Library/Saved Application State/net.pcsx2.pcsx2.savedState"
+  ]
+end
