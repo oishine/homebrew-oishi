e4a9044f1798a14a8e27efc834f5b75d306dee02
neroices <git@slce.moe>
2025-04-12 17:24:06 +0700

    pcsx2: Add verified url

--- Casks/pcsx2.rb
+++ Casks/pcsx2.rb
@@ -1,7 +1,8 @@
 cask "pcsx2" do
   version "2.3.260"
   sha256 "9ffd0f03899ecc3746edb2078d38b53f16eaca831fc0dc78c8988f5d9991f77d"
-  url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macos-Qt.tar.xz"
+  url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macos-Qt.tar.xz",
+    verified: "https://github.com/PCSX2/pcsx2/releases/download"
   name "PCSX2"
   desc "Open Source PS2 Emulator"
   homepage "https://pcsx2.net"
